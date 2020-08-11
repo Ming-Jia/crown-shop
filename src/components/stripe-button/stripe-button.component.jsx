97482c14de134acc6fa153c5adf0bd8c93cf0c35
Yong Ming-Jia <yongmingjia0112@gmail.com>
2020-08-11 13:41:29 +0800

    Lesson 13-2,5: Adding Stripe Payment and Updating favicon and Title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HEpmiEHEUExk34O1GAmJbgPZrquXrtuCXGo6QrGrB0R9SaX5NW2uqhktbK10Sn9VrIb5ASAOkpyTFYMqmPfKx7H00b9yv7z7I';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
