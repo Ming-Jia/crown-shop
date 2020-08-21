fde5b4745150f2555231e99adfd8b1a2ba116c63
Yong Ming-Jia <yongmingjia0112@gmail.com>
2020-08-21 16:40:12 +0800

    Fix Bugs on Home Page

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,10 @@ import Directory from '../../components/directory/directory.component';
 
 import { HomePageContainer } from './homepage.styles';
 
-const HomePage = () => {
+const HomePage = () => (
   <HomePageContainer>
     <Directory />
-  </HomePageContainer>;
-};
+  </HomePageContainer>
+);
 
 export default HomePage;
